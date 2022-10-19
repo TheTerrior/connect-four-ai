9b8a6b9122587921be90716d4f0bef36d6c69526
TheTerrior <werbird10@gmail.com>
2022-10-19 02:38:51 -0700

    Created the Rust project, began experimenting with wgpu

---
+++ src/main.rs
@@ -0,0 +1,8 @@
+use wgpu::{Instance, Backends};
+
+fn main() {
+    let instances = Instance::new(Backends::all());
+    for adapter in instances.enumerate_adapters(Backends::all()) {
+        println!("{:?}", adapter.get_info());
+    }
+}
